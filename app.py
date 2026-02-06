da860321cd2699009d5d826190f28aec979fa88d
jinnasaiteja <svas60058@gmail.com>
2026-02-06 08:27:08 -0500

    Added CI pipeline

--- app.py
+++ app.py
@@ -1,7 +1,52 @@
 from fastapi import FastAPI
+import psycopg2
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Hello DevOps Beginner"}
+def get_connection():
+    return psycopg2.connect(
+        host="db",
+        database="devopsdb",
+        user="postgres",
+        password="postgres"
+    )
+
+@app.on_event("startup")
+def create_table():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS products (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100),
+            price INT
+        );
+    """)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+@app.post("/products")
+def add_product(name: str, price: int):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO products (name, price) VALUES (%s, %s)",
+        (name, price)
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"message": "Product added"}
+
+@app.get("/products")
+def get_products():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM products")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
+
+
